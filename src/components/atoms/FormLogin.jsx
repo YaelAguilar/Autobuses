07d4b0f6a429f5d3026e5ed70ff04698d10f49f6
YaelAguilar <213452@ids.upchiapas.edu.mx>
2023-04-10 03:34:45 -0600

    [ADD] logo css

--- src/components/atoms/FormLogin.jsx
+++ src/components/atoms/FormLogin.jsx
@@ -2,6 +2,7 @@ import { Link } from 'react-router-dom';
 import { useNavigate } from 'react-router-dom';
 import { useRef } from 'react';
 import logo from "../../assets/img/transformers-autobot.svg";
+import "../../assets/style/formlogin.css";
 
 function FormLogin() {
     
