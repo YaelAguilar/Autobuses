a9d44617cd6ec1ac6a73db86af8dbd496a3bd3a3
YaelAguilar <213452@ids.upchiapas.edu.mx>
2023-04-10 05:51:59 -0600

    [ADD] import css

--- src/components/atoms/FormRegister.jsx
+++ src/components/atoms/FormRegister.jsx
@@ -1,5 +1,6 @@
 import { useRef } from "react";
 import { useNavigate } from "react-router-dom";
+import "../../assets/style/formregister.css";
 import logo from "../../assets/img/transformers-autobot.svg";
 
 function FormRegister() {
